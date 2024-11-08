aa9bbbff26f91a0ff0245453a0ca3f65de4b27c0
Kaua Davi <kauadaviamaro@gmail.com>
2024-11-08 06:23:57 -0300

    ADD login functionality

---
+++ Infrastructure/Repositories/AuthRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Models;
+using Core.Repositories;
+using Infrastructure.Repositories.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly GabiniDbContext _context;
+
+        public AuthRepository(GabiniDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetUserByEmailAndPassword(string email, string password)
+        {
+            return await _context.Users.FirstOrDefaultAsync(c => (c.Email == email || c.UserName == email) && c.Password == password);
+        }
+    }
+}
